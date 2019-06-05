c913cf0f6eab48f52a5177ecaf6f9efbf4504414
Matheus <Binotto>
2019-06-04 22:20:01 -0300

    Correcting Syntax - changing as braces {} by parentheses ()

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const HomePage = () => {
+const HomePage = () => (
     <div>  
         <h1>Welcome</h1>
         <p>This is my site. Take a look around!</p>
     </div>
-};
+);
 
 export default HomePage;
