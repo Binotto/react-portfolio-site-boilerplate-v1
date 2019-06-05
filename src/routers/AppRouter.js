f627f8c807effd417bea54798f5af44acda7e18f
Matheus <Binotto>
2019-06-04 22:09:52 -0300

    Importing a new Component - HomePage

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import NotFoundPage from '../components/NotFoundPage';
 import Header from '../components/Header';
+import HomePage from '../components/HomePage';
 
 
 const AppRouter = () => (
@@ -9,6 +10,7 @@ const AppRouter = () => (
         <div>
             <Header />
             <Switch>
+                <Route path ="/" component={HomePage}/>
                 <Route component={NotFoundPage} />
             </Switch>
         </div>
